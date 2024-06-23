5c9563774a3a57d74f7332d7daff6f10355c2f89
Cloud User <centos@vps-058d3d3a.vps.ovh.net>
2024-06-23 09:13:35 +0000

    fix volume db

--- scripts/prod/start.server.sh
+++ scripts/prod/start.server.sh
@@ -3,11 +3,10 @@
 parent_path=$( cd "$(dirname $(dirname $(dirname "${BASH_SOURCE[0]}")))" ; pwd -P )
 cd "$parent_path"
 
-docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml down --remove-orphans
 
-docker rmi ezred2api
+docker rmi ezred2api || true
 
 docker compose -f docker-compose.prod.yml build --no-cache --force-rm
-docker compose -f docker-compose.prod.yml up -d --force-recreate
 
-docker image prune --force
+docker compose -f docker-compose.prod.yml up -d --force-recreate
