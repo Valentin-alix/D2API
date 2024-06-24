f5366ba6a99cd4940577652bc5eddb136bf7ee04
Valentin Alix <valentin-alix.850@outlook.fr>
2024-06-24 14:58:57 +0200

    fix sub expire

--- scripts/populate/create_initial_user.py
+++ scripts/populate/create_initial_user.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from dotenv import get_key
 
 
@@ -9,7 +10,16 @@ from src.models.user import User
 def create_initial_user():
     username = get_key(ENV_PATH, "SUPERUSER_USERNAME")
     password = get_key(ENV_PATH, "SUPERUSER_PASSWORD")
+    if username is None or password is None:
+        raise ValueError("Missing SUPERUSER_USERNAME or SUPERUSER_PASSWORD in .env")
     session = SessionMaker()
-    if session.query(User).filter(User.email == username).one_or_none() is None:
-        session.add(User(email=username, password=password))
-        session.commit()
+    superuser = session.query(User).filter(User.email == username).one_or_none()
+    if superuser is None:
+        session.add(
+            User(email=username, password=password, sub_expire=datetime(3000, 1, 1))
+        )
+    else:
+        superuser.email = username
+        superuser.password = password
+        superuser.sub_expire = datetime(3000, 1, 1)
+    session.commit()
