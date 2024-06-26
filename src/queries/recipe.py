81b414221446bf26fed41c864ba3c27a69f65f79
Valentin Alix <valentin-alix.850@outlook.fr>
2024-06-26 11:04:24 +0200

    fix recipes

--- src/queries/recipe.py
+++ src/queries/recipe.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Row, func
+from sqlalchemy import Row, and_, func
 from sqlalchemy.orm import Session, aliased, joinedload
 
 from EzreD2Shared.shared.enums import CategoryEnum
@@ -17,10 +17,12 @@ def get_ordered_recipes(recipes: list[Recipe]):
     ordered_recipes: list[Recipe] = []
     for recipe in recipes:
         for ingredient in recipe.ingredients:
-            if ingredient.item_id not in (
-                elem.result_item_id for elem in ordered_recipes
-            ):
-                ordered_recipes.append(ingredient.recipe)
+            if ingredient.item.recipe not in recipes:
+                continue
+            if ingredient.item.recipe not in ordered_recipes:
+                ordered_recipes.append(ingredient.item.recipe)
+
+        ordered_recipes.append(recipe)
 
     return ordered_recipes
 
@@ -29,15 +31,43 @@ def get_ordered_recipes(recipes: list[Recipe]):
 def get_merged_ordered_with_recipe_items(
     session: Session, server_id: int, recipe_ids: list[int], item_ids: list[int]
 ) -> list[Item]:
+    total_ingredients_subquery = (
+        session.query(
+            Item.id,
+            func.count(Ingredient.id).label("total_ingredient_count"),
+        )
+        .join(Ingredient, Ingredient.item_id == Item.id)
+        .group_by(Item.id)
+        .subquery()
+    )
+    filtered_ingredients_subquery = (
+        session.query(
+            Item.id,
+            func.count(Ingredient.id).label("filtered_ingredient_count"),
+        )
+        .join(
+            Ingredient,
+            and_(
+                Ingredient.item_id == Item.id, Ingredient.recipe_id.not_in(recipe_ids)
+            ),
+        )
+        .group_by(Item.id)
+        .subquery()
+    )
+
     return (
         session.query(Item)
         .join(Ingredient, Ingredient.item_id == Item.id)
-        .join(Recipe, Ingredient.recipe_id == Recipe.id)
         .join(Price, Price.item_id == Item.id)
+        .join(total_ingredients_subquery, total_ingredients_subquery.c.id == Item.id)
+        .join(
+            filtered_ingredients_subquery, filtered_ingredients_subquery.c.id == Item.id
+        )
         .filter(
-            Recipe.id.not_in(recipe_ids),
             Item.id.in_(item_ids),
             Price.server_id == server_id,
+            total_ingredients_subquery.c.total_ingredient_count
+            == filtered_ingredients_subquery.c.filtered_ingredient_count,
         )
         .order_by(Price.average.desc())
         .all()
@@ -48,6 +78,26 @@ def get_merged_ordered_with_recipe_items(
 def get_recipes_to_upgrade_jobs(session: Session, character: Character) -> set[Recipe]:
     recipes: set[Recipe] = set()
     bank_item_ids: list[int] = [item.id for item in character.bank_items]
+
+    total_ingredients_subquery = (
+        session.query(
+            Ingredient.recipe_id,
+            func.count(Ingredient.id).label("total_ingredient_count"),
+        )
+        .group_by(Ingredient.recipe_id)
+        .subquery()
+    )
+
+    filtered_ingredients_subquery = (
+        session.query(
+            Ingredient.recipe_id,
+            func.count(Ingredient.id).label("filtered_ingredient_count"),
+        )
+        .filter(Ingredient.item_id.in_(bank_item_ids))
+        .group_by(Ingredient.recipe_id)
+        .subquery()
+    )
+
     for job_info in character.harvest_jobs_infos:
         if job_info.lvl == 200:
             continue
@@ -56,13 +106,22 @@ def get_recipes_to_upgrade_jobs(session: Session, character: Character) -> set[R
             session.query(Recipe)
             .join(Item, Item.id == Recipe.result_item_id)
             .join(Ingredient, Recipe.id == Ingredient.recipe_id)
+            .join(
+                filtered_ingredients_subquery,
+                Recipe.id == filtered_ingredients_subquery.c.recipe_id,
+            )
+            .join(
+                total_ingredients_subquery,
+                Recipe.id == total_ingredients_subquery.c.recipe_id,
+            )
             .filter(
                 Recipe.job_id == job_info.job_id,
                 Item.level.between(job_info.lvl - 40, job_info.lvl),
+                total_ingredients_subquery.c.total_ingredient_count
+                == filtered_ingredients_subquery.c.filtered_ingredient_count,
             )
+            .group_by(Recipe.id)
             .options(joinedload(Recipe.ingredients).subqueryload(Ingredient.item))
-            .group_by(Recipe.id, Ingredient.item_id)
-            .having(Ingredient.item_id.in_(bank_item_ids))
             .all()
         )
         recipes.update(recipes_job)
