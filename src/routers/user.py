3ab8ecfc8ff3486f48c9b6f6f52f359bf8bc3ba7
Valentin Alix <valentin-alix.850@outlook.fr>
2024-09-18 16:32:02 +0200

    add create user for public

--- src/routers/user.py
+++ src/routers/user.py
@@ -1,13 +1,16 @@
 import datetime
 from typing import Annotated
+
+from dateutil.relativedelta import relativedelta
 from fastapi import APIRouter, Depends
 from fastapi.security import HTTPBasicCredentials
 from sqlalchemy.orm import Session
+
 from D2Shared.shared.schemas.user import CreateUserSchema, ReadUserSchema
+from src.database import session_local
 from src.models.user import User
-from dateutil.relativedelta import relativedelta
 from src.queries.user import populate_config_user
-from src.security.auth import login, login_for_admin
+from src.security.auth import login
 
 router = APIRouter(prefix="/users")
 
@@ -19,13 +22,15 @@ def get_user_me(user: Annotated[HTTPBasicCredentials, Depends(login)]):
 
 @router.post("/", response_model=ReadUserSchema)
 def create_user(
-    user_datas: CreateUserSchema, session: Session = Depends(login_for_admin)
+    user_datas: CreateUserSchema, session: Session = Depends(session_local)
 ):
-    user = User(
-        **user_datas.model_dump(),
-        sub_expire=datetime.datetime.now() + relativedelta(years=1),
-    )
-    session.add(user)
-    session.commit()
-    populate_config_user(session, user.id)
-    return user
+    related_user = session.query(User).filter(User.email == user_datas.email).first()
+    if related_user is None:
+        related_user = User(
+            **user_datas.model_dump(),
+            sub_expire=datetime.datetime.now() + relativedelta(years=1),
+        )
+        session.add(related_user)
+        session.commit()
+        populate_config_user(session, related_user.id)
+    return related_user
