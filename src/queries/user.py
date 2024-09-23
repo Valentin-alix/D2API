9e09b84151bc36c11969d24b5baea055d5c5efc4
Valentin Alix <valentin-alix.850@outlook.fr>
2024-09-23 21:51:24 +0200

    remove default playtime

--- src/queries/user.py
+++ src/queries/user.py
@@ -1,13 +1,7 @@
-from datetime import time
 from sqlalchemy.orm import Session
 
-from src.models.user import ConfigUser, RangeHourPlayTime, RangeWait
+from src.models.user import ConfigUser, RangeWait
 from src.queries.utils import get_or_create
-
-
-DEFAULT_RANGES_HOURS_PLAYTIME: list[tuple[time, time]] = [
-    (time(hour=8), time(hour=23, minute=30))
-]
 
 
 def populate_config_user(session: Session, user_id: int):
@@ -23,12 +17,3 @@ def populate_config_user(session: Session, user_id: int):
     config_user = ConfigUser(range_new_map=range_new_map, user_id=user_id)
     session.add(config_user)
     session.commit()
-
-    for range_hour in DEFAULT_RANGES_HOURS_PLAYTIME:
-        get_or_create(
-            session,
-            RangeHourPlayTime,
-            start_time=range_hour[0],
-            end_time=range_hour[1],
-            config_user=config_user,
-        )
